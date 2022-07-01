00f834935bdcbf536940df87da5d983314acf1a6
Danish ali <danishali.brs@gmail.com>
2022-07-02 04:42:34 +0500

    class-assigment

---
+++ app.js
@@ -0,0 +1,59 @@
+// var name = "Danish";
+// var age = "22";
+// var certificate = "Certified Mobile Application Development";
+// var email = "danishali.brs@gmail.com";
+// var book = "A smarter way to learn JavaScript";
+// var visits = "You have visited this site 18 times"
+// alert(name);
+// alert(age);
+// alert(certificate);
+// alert(email);
+// alert(book);
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+// alert(visits);
+// document.write("Yah! I can write HTML content through Javascript");
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+//chapter 2 complete
+//
+
+// document.write("My birth day year is 1990" , "<br/>" , "Data type of my declared variable number");
+// document.write("<b>John Doe</b>ordered <b>5 T-shirt</b>(s) on XYZ Clothing store");
+
+//chapter 3 complete
+
+// document.write("<b>Rules for naming JS variables</b>","<br/>", "<br/>","<br/>", "Variable names can only contain , numbers, $ and_. For example : $my_1stVariable ", "<br/>" , "Variable must begin with a letter, $ or_. For example : $name, _name or name" , "<br/>" , "Variable name are case sensitive" , "<br/>" ,"Variable name should not be JS keywords")
+
+// chapter 4 complete
+
+// var cityName = "Karachi";
+
+// var x = prompt("Enter city Name");
+
+// if (x == cityName) {alert("Welcome to the city of lights")
+    
+// }
+
+// var genderA = "Male"
+// var genderB = "Female"
+
+// var o = prompt("Enter your gender")
+
+
+// if (o == genderA) {alert("Good morning sir");
+
+// }
+
+// if (o == genderB) {alert("Good morning Mam");
+
+// }
+
+
+
+
+
+
+
+
+
+
+
