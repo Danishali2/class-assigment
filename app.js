d89aa070dcdf39e59c8b566da2730fa6052925d3
Danish ali <danishali.brs@gmail.com>
2022-07-04 02:49:29 +0500

    class-assigment

--- app.js
+++ app.js
@@ -1,57 +1,151 @@
-var name = "Danish";
-var age = "22";
-var certificate = "Certified Mobile Application Development";
-var email = "danishali.brs@gmail.com";
-var book = "A smarter way to learn JavaScript";
-var visits = "You have visited this site 18 times"
-alert(name);
-alert(age);
-alert(certificate);
-alert(email);
-alert(book);
-alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
-alert(visits);
-document.write("Yah! I can write HTML content through Javascript");
-alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
-//chapter 2 complete
-//
+// var name = "Danish";
+// var age = "22";
+// var certificate = "Certified Mobile Application Development";
+// var email = "danishali.brs@gmail.com";
+// var book = "A smarter way to learn JavaScript";
+// var visits = "You have visited this site 18 times"
+// alert(name);
+// alert(age);
+// alert(certificate);
+// alert(email);
+// alert(book);
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+// alert(visits);
+// document.write("Yah! I can write HTML content through Javascript");
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+// //chapter 2 complete
+// //
 
-document.write("My birth day year is 1990" , "<br/>" , "Data type of my declared variable number");
-document.write("<b>John Doe</b>ordered <b>5 T-shirt</b>(s) on XYZ Clothing store");
+// document.write("My birth day year is 1990" , "<br/>" , "Data type of my declared variable number");
+// document.write("<b>John Doe</b>ordered <b>5 T-shirt</b>(s) on XYZ Clothing store");
 
-//chapter 3 complete
+// //chapter 3 complete
 
-document.write("<b>Rules for naming JS variables</b>","<br/>", "<br/>","<br/>", "Variable names can only contain , numbers, $ and_. For example : $my_1stVariable ", "<br/>" , "Variable must begin with a letter, $ or_. For example : $name, _name or name" , "<br/>" , "Variable name are case sensitive" , "<br/>" ,"Variable name should not be JS keywords")
+// document.write("<b>Rules for naming JS variables</b>","<br/>", "<br/>","<br/>", "Variable names can only contain , numbers, $ and_. For example : $my_1stVariable ", "<br/>" , "Variable must begin with a letter, $ or_. For example : $name, _name or name" , "<br/>" , "Variable name are case sensitive" , "<br/>" ,"Variable name should not be JS keywords")
 
-// chapter 4 complete
+// // chapter 4 complete
 
-var cityName = "Karachi";
+// var cityName = "Karachi";
 
-var x = prompt("Enter city Name");
+// var x = prompt("Enter city Name");
 
-if (x == cityName) {alert("Welcome to the city of lights")
+// if (x == cityName) {alert("Welcome to the city of lights")
+    
+// }
+
+// var genderA = "Male"
+// var genderB = "Female"
+
+// var o = prompt("Enter your gender")
+
+
+// if (o == genderA) {alert("Good morning sir");
+
+// }
+
+// if (o == genderB) {alert("Good morning Mam");
+
+// }
+
+// const num1 = 5;
+// const num2 = 3;
+
+// // add two numbers
+// const sum = num1 + num2;
+
+// // display the sum
+// document.write('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+
+// var num = prompt("Enter Number", "0") //prompt user to enter the number
+// var num = parseInt(num); //parse the num to number
+// var i = 0;
+// document.write('<table border="1" cellspacing="0">');
+// for (i = 1; i < 10; i++) {
+//   document.write("<tr><td>" + num + " x " + i + " = " + num * i + "</td></tr>");
+// }
+
+// var s1,s2,s3,s4,s5,s6,s7,total,per,name,roll;
+//     name=prompt("Enter the student name");
+//     s1=parseInt(prompt("enter subject1 mark    : " ));
+//     s2=parseInt(prompt("enter subject2 mark    : " ));
+//     s3=parseInt(prompt("enter subject3 mark    : " ));
+//     s4=parseInt(prompt("enter subject4 mark    : " ));
+//     s5=parseInt(prompt("enter subject5 mark    : " ));
+//     s6=parseInt(prompt("enter subject6 mark    : " ));
+//     s7=parseInt(prompt("enter subject7 mark    : " ));
+
+//     total=s1+s2+s3+s4+s5+s6+s7;
+//     per=total/7;
+
+//     document.write(" <h1>    <center>   MARKSHEET    </h1> </center>");
+//     document.write(" <hr color=red>   <hr color=red>  ");
+//     document.write(" <br> Name          :  " + name  +"</h3>");
+//     document.write(" <hr color=black>");
+//        document.write(" <b> <br> &nbsp &nbsp &nbsp     Subject           &nbsp &nbsp&nbsp &nbsp Mark  </b>");
+
+//     document.write(" <br> &nbsp &nbsp &nbsp     Subject 1      &nbsp &nbsp&nbsp &nbsp     &nbsp &nbsp   " + s1);
+//     document.write(" <br> &nbsp &nbsp &nbsp     Subject 2        &nbsp &nbsp&nbsp &nbsp   &nbsp &nbsp   "  + s2);
+//     document.write(" <br> &nbsp &nbsp &nbsp     Subject 3      &nbsp &nbsp&nbsp &nbsp    &nbsp &nbsp   " + s3);
+//     document.write(" <br> &nbsp &nbsp &nbsp     Subject 4       &nbsp &nbsp&nbsp &nbsp   &nbsp &nbsp    " + s4);
+//     document.write(" <br> &nbsp &nbsp &nbsp     Subject 5       &nbsp &nbsp&nbsp &nbsp   &nbsp &nbsp   " + s5);
+//     document.write(" <br> &nbsp &nbsp &nbsp     Subject 6        &nbsp &nbsp&nbsp &nbsp   &nbsp &nbsp   " + s6);
+//     document.write(" <br> &nbsp &nbsp &nbsp     Subject 7        &nbsp &nbsp&nbsp &nbsp  &nbsp &nbsp    " + s7);
+//     if(s1<40 || s2<40 || s3<40 || s4<40 || s5<40 || s6<40 || s7<40)
+//     {
+//         document.write(" <hr color=black>");
+//         document.write("  Total               <b>:  " + total  +  "<br>");   
+//         document.write(" &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <font color=red  > <h3>Fail  </h3></font>");
+//     }
+//     else
+//     {
+//         document.write(" <hr color=black>");
+//         document.write("    Total               :  " + total);   
+//         document.write("   <br>  Percentage   :  " + per);   
+//         document.write("   <br> grade                      :");
+
+//         if(per>=70)
+//         {
+//             document.write("      <b>Districation");
+//         }
+//         else if(per >=60)
+//         {
+//             document.write("       <b>First class");
+//         }
+//         else if(per>=48)
+//         {
+//             document.write("       <b>Second class");
+//         }
+//         else
+//         {
+//             document.write("        <b>pass class");
+//         }            
+//     }
+//     document.write(" <hr color=red>   <hr color=red>  ");
+
+    //Chapter 6-9 complete
+
+// var u = "Karachi"
+
+// var customer = prompt("Enter your city");
+
+// if (y => customer) {alert("welcome to the city of light")
+    
+// }
+   
+var colour1 = "red";
+var colour2 = "yellow";
+var colour3 = "green";
+
+
+var person = prompt("which color are seeing in trafic signal ");
+
+if (colour1 == person) {alert("Stop");
+   
+} else if (colour2 == person) {alert("move");
+   
+} else if (colour3 == person) {alert("move now")
     
 }
-
-var genderA = "Male"
-var genderB = "Female"
-
-var o = prompt("Enter your gender")
-
-
-if (o == genderA) {alert("Good morning sir");
-
-}
-
-if (o == genderB) {alert("Good morning Mam");
-
-}
-
-
-
-
-
-
 
 
 
